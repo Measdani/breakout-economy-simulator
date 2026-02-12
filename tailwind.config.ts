e08da868a8f6d02aa54acc3806829d4c6a82a146
Developer <user@example.com>
2026-02-12 09:53:56 -0500

    Add PostCSS and Tailwind config files

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        solvent: '#10b981', // green
+        deficit: '#ef4444', // red
+      },
+    },
+  },
+  plugins: [],
+};
+
+export default config;
